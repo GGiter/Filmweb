392e40de065633ee2b721a941af3da74fdcbb511
Piotr Gębuś <piotrek194@poczta.fm>
2019-12-31 17:19:18 +0100

    add review tests

--- data_objects/review.py
+++ data_objects/review.py
@@ -1,9 +1,9 @@
 
 class Review():
     def __init__(self,user_id,movie_id,score,id = None):
-        self.user_id = user_id
-        self.movie_id = movie_id
-        self.score = score
+        self.user_id = user_id if isinstance(user_id,int) else 0
+        self.movie_id = movie_id if isinstance(movie_id,int) else 0
+        self.score = score if isinstance(score,int) else 0
 
     def get_user_id(self):
         return self.user_id
---
+++ tests/test_review.py
@@ -0,0 +1,13 @@
+from data_objects.review import Review
+
+def test_user_id():
+    review = Review("","","")
+    assert review.get_user_id() == 0
+
+def test_movie_id():
+    review = Review("","","")
+    assert review.get_movie_id() == 0
+
+def test_score_id():
+    review = Review("","","")
+    assert review.get_score() == 0
